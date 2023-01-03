dc15fa1de47f0846e2925348898ba933e824f09e
Zakria-Akram <zakria.akram99@gmail.com>
2023-01-03 09:50:47 +0500

    final one

---
+++ controllers/shop.js
@@ -0,0 +1,54 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      console.log(products);
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getProductById = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getAboutPage = (req, res, next) => {
+  res.render("shop/about", {
+    pageTitle: "About Us",
+    path: "/about",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
